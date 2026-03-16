d1efd41e4b6b2d52afd066b51e60116a8416eaa3
Shivam Anand <shivamanand791@gmail.com>
2026-03-16 21:55:26 +0530

    Robust CORS origin normalization

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const allowedOrigins = [
   "http://localhost:5500",
   "http://127.0.0.1:5500",
   process.env.FRONTEND_URL
-].filter(Boolean);
+].filter(Boolean).map(url => url.replace(/\/$/, "")); // Remove trailing slash
+
 
 app.use(cors({
   origin: (origin, callback) => {
